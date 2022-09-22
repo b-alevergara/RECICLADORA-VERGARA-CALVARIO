127d119f127e7e758a45448b598f278823e3070a
b-alevergara <belenvergara97@gmail.com>
2022-09-21 23:39:34 -0500

    ajustes que corregi en mi evento

--- formulario.js
+++ formulario.js
@@ -1,22 +1,17 @@
 //evento submit para mandar la info capturada del form
-let enviar=document.getElementById("botonEnviar")
-miFormulario.addEventListener("submit", validacion);
-
-function validacion(evento) {
-evento.preventDefault();
-console.log(evento);
-console.log("El formulario ha sido enviado");
-}
-
-//inicio elementos
+//e inicio elementos
 function enviarFormulario(){
+    const enviar=document.getElementById("botonEnviar")
+    enviar.addEventListener("submit", (evento)=>{
+        evento.preventDefault();
+    
     let nombre=document.getElementById("capturaNombre").value;
     let email=document.getElementById("capturaEmail").value;
     let seleccion=document.getElementById("seleccionTema").value;
     let comentarios=document.getElementById("comentarios").value;
     //un ciclo para validar que los campos son llenados correctamente
     if(nombre==""){
-        alert("El e-mail es un campo obligatorio.");
+        alert("El nombre es un campo obligatorio.");
         document.getElementById("capturaNombre").focus();
     }else{
         if(email==""){
@@ -24,22 +19,13 @@ function enviarFormulario(){
         document.getElementById("capturaEmail").focus();
     }else{
         console.log(nombre +""+ email+""+seleccion+""+comentarios)
-        // document.getElementById("capturaNombre").value=""
-        // document.getElementById("capturaEmail").value=""
-        // document.getElementById("seleccionTema").value=""
-        // document.getElementById("comentarios").value=""
-        // document.getElementById("capturaNombre").focus();
-    }
-    }
-}
-//inicio evento
-// function iniciarEvento() {
-
-
-    // form.onsubmit = (event) => validarFormulario(event);
-//   }
-// function main(){
-//     enviarFormulario();
-//     // iniciarEvento();
-// }
-// main();
+        document.getElementById("capturaNombre").value=""
+        document.getElementById("capturaEmail").value=""
+        document.getElementById("seleccionTema").value=""
+        document.getElementById("comentarios").value=""
+        //para que vuelvan a iniciar el formulario
+        document.getElementById("capturaNombre").focus();
+    }}
+    })}
+function main(){enviarFormulario();}
+main()
